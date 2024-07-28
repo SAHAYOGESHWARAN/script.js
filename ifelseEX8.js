11f138acbee6788577e8fb93f719b5bed2c31fed
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-28 16:57:41 +0530

    add script master

--- ifelseEX8.js
+++ ifelseEX8.js
@@ -1,13 +1,16 @@
-var score ="60"
+var score ="90"
 
 if(score<50)
 {
     console.log("you need to improve")
 }
-if(score>50 && score<70)
+if(score>50 && score<=70)
 {
     console.log("good job")
 }
+if(score>70)
+{
+    console.log("Excelent")
+}
 
-
-// good job
+//Excelent 
