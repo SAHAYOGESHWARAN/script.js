b179ed03c1f0a89fa61de240ff3ab287bac0deca
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-28 12:23:20 +0530

    add script master

---
+++ exercise3.js
@@ -0,0 +1,12 @@
+var fbike= "R15v3";
+var fclr="blue";
+var fcamera= "insta360";
+
+function mypersonal()
+{
+    console.log("My fav bike are:"+fbike);
+    console.log("My fav color are:"+fclr);
+    console.log("my fav camera:"+fcamera);
+}
+
+mypersonal()
