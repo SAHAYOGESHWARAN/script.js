b734a9415c1b50669e40e1e8c8a29f4977087871
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-28 12:11:00 +0530

    add script master

---
+++ exercise2.js
@@ -0,0 +1,12 @@
+var factor = "kamal";
+var fplayer ="dhoni";
+var fmovie ="leo";
+
+function favourite()
+{
+    console.log("My favourite player is: " + fplayer);
+    console.log("My favourite actor is: " + factor);
+    console.log("My favourite movie is: " + fmovie);
+    
+}
+favourite()
