9e6951ccd5909b9c3d0d0b0c4927dcd0f3d36aca
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-28 16:44:10 +0530

    add script master

---
+++ ifelseEx7.js
@@ -0,0 +1,20 @@
+var seasons = "spring"
+
+if(seasons =="spring" )
+{
+    console.log("enjoy the blooming flowers");
+}
+if(seasons =="summer")
+{
+    console.log("have fun in the sun")
+}
+if(seasons =="autumn")
+{
+    console.log("Admire the colourful leaves")
+}
+if(seasons =="winter")
+{
+    console.log("Bundle up and stay warm")
+}
+
+//enjoy the blooming flowers
